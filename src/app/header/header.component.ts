083d6b15aa871d80df87f1d3d1c4ee26fc069c44
Zaireko <c.aldahir.jc@gmail.com>
2023-09-01 10:27:05 -0600

    add vars header #3

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+name : string = "fake name";
+titulo : string = "fake phd";
+objetivo : string = "fake goal";
+foto : string = "fake photo";
+email : string = "fake email";
+celular : string = "fake cel";
+ubicacion : string = "fake location"
+redsocial : string = "fake social";
 }
