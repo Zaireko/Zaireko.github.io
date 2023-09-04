6067f75986a95b69faf04824bec19e824aa9e18f
Zaireko <c.aldahir.jc@gmail.com>
2023-09-04 11:15:35 -0600

    combinacion de api y front #4

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private headerService: HeaderService) { }
+
 name : string = "fake name";
 titulo : string = "fake phd";
 objetivo : string = "fake goal";
 foto : string = "fake photo";
 email : string = "fake email";
-celular : string = "fake cel";
+celular : string = "fake cel";  
 ubicacion : string = "fake location"
 redsocial : string = "fake social";
+
+ngOnInit() : void {
+  this.headerService.getHeader().subscribe( (data : any) =>{
+    console.log(data);
+    alert(data);
+
+    this.name = data.name;
+    this.titulo = data.titulo;
+    this.objetivo = data.objetivo;
+    this.foto = data.foto;
+    this.email = data.email;
+    this.celular = data.celular;
+    this.ubicacion = data.ubicacion;
+    this.redsocial = data.redsocial;
+  });
+}
+
+
 }
