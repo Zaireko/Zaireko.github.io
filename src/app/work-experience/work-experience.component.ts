b20c7b52a73af338c768ab05168057c2e4917050
Zaireko <c.aldahir.jc@gmail.com>
2023-09-07 21:00:24 -0600

    crear rama work-experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.css']
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
 
+  constructor(private workexService: WorkExperienceService) { }
+
+  puesto : string = "fake tittle";
+  empresa : string = "fake emprise";
+  fecha_ini : string = "fake init date";
+  fecha_fin : string = "fake end date";
+  ciudad : string = "fake city";
+  pais : string = "fake country";
+  logros: string[] = [];
+
+  ngOnInit() {
+    this.workexService.getWE().subscribe((data: any) => {
+      console.log(data);
+      alert(data);
+
+      this.puesto = data.puesto;
+      this.empresa = data.empresa;
+      this.fecha_ini = data.fecha_ini;
+      this.fecha_fin = data.fecha_fin;
+      this.ciudad = data.ciudad;
+      this.pais = data.pais;
+      this.logros = data.logros;
+    });
+  }
 }
