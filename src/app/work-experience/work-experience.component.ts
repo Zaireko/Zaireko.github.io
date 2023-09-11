dd7e493f969ed95730f7245fdcb08ab7852cd897
Zaireko <135562677+Zaireko@users.noreply.github.com>
2023-09-11 09:40:41 -0600

    Develop (#14)
    * Update develop_CI_CD.yml (#9)
    
    * Work experience (#12)
    
    * crear rama work-experience
    
    * work experience feo en tabla
    
    * correccion de work-experience convirtiendolo en un arreglo
    
    * extra

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.css']
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
 
+  constructor(private workexService: WorkExperienceService) { }
+
+  experiences: any[] = [];
+
+  ngOnInit() {
+    this.workexService.getWE().subscribe((data: any[]) => {
+      console.log(data);
+
+      // Asigna los valores obtenidos desde el backend a la propiedad experiences
+      this.experiences = data;
+    });
+  }
 }
