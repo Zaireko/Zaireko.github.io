e2ea5bd7f46ee5aadb22414d64f41037b81b9337
Zaireko <c.aldahir.jc@gmail.com>
2023-09-11 08:45:07 -0600

    correccion de work-experience convirtiendolo en un arreglo

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -10,26 +10,14 @@ export class WorkExperienceComponent implements OnInit {
 
   constructor(private workexService: WorkExperienceService) { }
 
-  puesto : string = "fake tittle";
-  empresa : string = "fake emprise";
-  fecha_ini : string = "fake init date";
-  fecha_fin : string = "fake end date";
-  ciudad : string = "fake city";
-  pais : string = "fake country";
-  logros: any[] = [];
+  experiences: any[] = [];
 
   ngOnInit() {
-    this.workexService.getWE().subscribe((data: any) => {
+    this.workexService.getWE().subscribe((data: any[]) => {
       console.log(data);
-      alert(data);
 
-      this.puesto = data.puesto;
-      this.empresa = data.empresa;
-      this.fecha_ini = data.fecha_ini;
-      this.fecha_fin = data.fecha_fin;
-      this.ciudad = data.ciudad;
-      this.pais = data.pais;
-      this.logros = data.logros;
+      // Asigna los valores obtenidos desde el backend a la propiedad experiences
+      this.experiences = data;
     });
   }
 }
